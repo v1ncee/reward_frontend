f10e6f965d1e5785da40a007a19fa46a7e30eedd
CezaryFradczak <r0658035@student.thomasmore.be>
2018-11-20 13:09:05 +0100

    Navbar admin routes - rewards-admin

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,8 +4,11 @@ import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { AuthGuard } from './_guards';
-import {RewardsOverviewComponent} from "./rewards-overview/rewards-overview.component";
-import {PurchasesOverviewComponent} from "./purchases-overview/purchases-overview.component";
+import {RewardsOverviewComponent} from './rewards-overview/rewards-overview.component';
+import {PurchasesOverviewComponent} from './purchases-overview/purchases-overview.component';
+import {RewardsAdminComponent} from './rewards-admin/rewards-admin.component';
+import {ApplicationsAdminComponent} from './applications-admin/applications-admin.component';
+import {UsersAdminComponent} from './users-admin/users-admin.component';
 
 
 const appRoutes: Routes = [
@@ -15,6 +18,9 @@ const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'rewards', component: RewardsOverviewComponent },
     { path: 'purchases', component: PurchasesOverviewComponent },
+    { path: 'rewards-admin', component: RewardsAdminComponent},
+    { path: 'applications-admin', component: ApplicationsAdminComponent},
+    { path: 'users-admin', component: UsersAdminComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
