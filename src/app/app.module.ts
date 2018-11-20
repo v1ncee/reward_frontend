c8bc8661b8f38cc9ac475cfc68bc3bc0975b0422
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-20 13:23:51 +0100

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,26 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {NgBootstrapModule} from './sharedModules/ng-bootstrap.module';
-import { NavbarComponent } from './sharedComponents/navbar/navbar.component';
-import { FooterComponent } from './sharedComponents/footer/footer.component';
-import { ReactiveFormsModule }    from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {NavbarComponent} from './sharedComponents/navbar/navbar.component';
+import {FooterComponent} from './sharedComponents/footer/footer.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
+import {AppComponent} from './app.component';
+import {routing} from './app.routing';
 
-import { AlertComponent } from './_directives';
-import { AuthGuard } from './_guards';
-import { JwtInterceptor } from './_helpers';
-import { AlertService, AuthenticationService, UserService } from './_services';
-import { HomeComponent } from './home';
-import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
-import { RewardsOverviewComponent } from './rewards-overview/rewards-overview.component';
-import { RewardItemComponent } from './rewards-overview/reward-item/reward-item.component';
-import { ExercisesOverviewComponent } from './exercises-overview/exercises-overview.component';
-import { PurchasesOverviewModule } from './purchases-overview/purchases-overview.module';
+import {AlertComponent} from './_directives';
+import {AuthGuard} from './_guards';
+import {JwtInterceptor} from './_helpers';
+import {AlertService, AuthenticationService, UserService} from './_services';
+import {HomeComponent} from './home';
+import {LoginComponent} from './login';
+import {RegisterComponent} from './register';
+import {RewardsOverviewComponent} from './rewards-overview/rewards-overview.component';
+import {RewardItemComponent} from './rewards-overview/reward-item/reward-item.component';
+import {ExercisesOverviewComponent} from './exercises-overview/exercises-overview.component';
+import {PurchasesOverviewModule} from './purchases-overview/purchases-overview.module';
+import {RewardsAdminComponent} from "./rewards-admin/rewards-admin.component";
 
 
 @NgModule({
@@ -45,13 +46,14 @@ import { PurchasesOverviewModule } from './purchases-overview/purchases-overview
     RewardsOverviewComponent,
     RewardItemComponent,
     ExercisesOverviewComponent,
+    RewardsAdminComponent
   ],
   providers: [
     AuthGuard,
     AlertService,
     AuthenticationService,
     UserService,
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,8 +7,8 @@ import { AuthGuard } from './_guards';
 import {RewardsOverviewComponent} from './rewards-overview/rewards-overview.component';
 import {PurchasesOverviewComponent} from './purchases-overview/purchases-overview.component';
 import {RewardsAdminComponent} from './rewards-admin/rewards-admin.component';
-import {ApplicationsAdminComponent} from './applications-admin/applications-admin.component';
-import {UsersAdminComponent} from './users-admin/users-admin.component';
+// import {ApplicationsAdminComponent} from './applications-admin/applications-admin.component';
+// import {UsersAdminComponent} from './users-admin/users-admin.component';
 
 
 const appRoutes: Routes = [
@@ -19,8 +19,8 @@ const appRoutes: Routes = [
     { path: 'rewards', component: RewardsOverviewComponent },
     { path: 'purchases', component: PurchasesOverviewComponent },
     { path: 'rewards-admin', component: RewardsAdminComponent},
-    { path: 'applications-admin', component: ApplicationsAdminComponent},
-    { path: 'users-admin', component: UsersAdminComponent},
+    // { path: 'applications-admin', component: ApplicationsAdminComponent},
+    // { path: 'users-admin', component: UsersAdminComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
