21f139f6ce991131a767bde0e5280547f4ac87d2
CezaryFradczak <r0658035@student.thomasmore.be>
2018-11-21 15:16:34 +0100

    api-rewards-service

--- src/app/_services/api-rewards.service.ts
+++ src/app/_services/api-rewards.service.ts
@@ -21,13 +21,23 @@ export class ApiRewardsService {
     return this.http.get(this.URLgetAllRewards)
       .toPromise().then(data => {
         return data;
-      })
+      });
   }
 
   getRewardById(id) {
     return this.http.get(this.URLgetRewardById + id)
       .toPromise().then(data => {
         return data;
-      })
+      });
+  }
+
+  updateReward(reward) {
+    return this.http.put(this.URLgetAllRewards + reward.id, reward);
+  }
+  addReward(reward) {
+    return this.http.post(this.URLgetAllRewards + reward.id, reward);
+  }
+  deleteReward(id) {
+    return this.http.delete(this.URLgetAllRewards + id);
   }
 }
