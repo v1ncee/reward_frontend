0a768bc67624c7f6f53ec4408a566ec0b789cab3
CezaryFradczak <r0658035@student.thomasmore.be>
2018-11-21 11:37:24 +0100

    rewards-admin, application-admin

--- src/app/applications-admin/applications-admin.component.ts
+++ src/app/applications-admin/applications-admin.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./applications-admin.component.sass']
 })
 export class ApplicationsAdminComponent implements OnInit {
-
+  items = [
+    {name: 'Test', date: '24/11/2018', description: 'This is a test description'},
+    {name: 'Test', date: '24/11/2018', description: 'This is a test description'},
+    {name: 'Test', date: '24/11/2018', description: 'This is a test description'},
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  accept(item) {
+    console.log(item);
+  }
+  decline(item) {
+    console.log(item);
+  }
 }
--- src/app/rewards-admin/rewards-admin.component.ts
+++ src/app/rewards-admin/rewards-admin.component.ts
@@ -10,6 +10,10 @@ export class RewardsAdminComponent implements OnInit {
     {name: 'Aruba', price: 3000, image: 'https://media.tuicontent.nl/p/header/vakantie-aruba.jpg', active: false},
     {name: 'Opleiding', price: 200, image: 'https://media.tuicontent.nl/p/header/vakantie-aruba.jpg', active: false},
     {name: 'Test', price: 25, image: 'https://media.tuicontent.nl/p/header/vakantie-aruba.jpg', active: false},
+    {name: 'Bier', price: 25, image: 'https://media.tuicontent.nl/p/header/vakantie-aruba.jpg', active: false},
+    {name: 'Trip to North Korea', price: 'FREE', image: 'https://media.tuicontent.nl/p/header/vakantie-aruba.jpg', active: false},
+    {name: 'JAVA', price: 'DEAD', image: 'http://3.bp.blogspot.com/-kUT0WNNNTLo/TrUbE-gvyUI/AAAAAAAABHU/wF_0X7Hs258/s1600/javadead.png', active: false},
+
   ];
   constructor() { }
 
