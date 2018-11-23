2c44d7cbd1be0ce3261bdb5b05b0e0db4e7f900b
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-23 11:05:12 +0100

    purchases history

--- src/app/purchases-overview/purchases-overview.component.ts
+++ src/app/purchases-overview/purchases-overview.component.ts
@@ -3,6 +3,8 @@ import { ApidbService } from '../services/apidb.service';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import {UserService} from "../_services";
+import {of} from "rxjs/index";
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-puchases',
@@ -13,11 +15,19 @@ export class PurchasesOverviewComponent implements OnInit {
 
   loading = false;
   purchases;
+  user$: Observable<any>;
   constructor(private apidbService: ApidbService, private userService: UserService) {
   }
 
   ngOnInit() {
     this.getPurchases();
+
+    //load user
+    const jwtHelper = new JwtHelperService();
+    if (localStorage.getItem('currentUser')) {
+      const id = jwtHelper.decodeToken(JSON.parse(localStorage.getItem('currentUser')).token).sub;
+      this.userService.getById(id).then(data => this.user$ = of(data));
+    }
   }
 
   getPurchases() {
